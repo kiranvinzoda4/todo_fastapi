e0085a9a8feb1e83e6c9f9745db542c88ec1044a
kiranvinzoda4 <101656754+kiranvinzoda4@users.noreply.github.com>
2023-09-17 09:11:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://kiran:kiran@localhost:3306/fastapi_app"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:kiran@localhost:3306/fastapi_app"
 # SQLALCHEMY_DATABASE_URL = "mysql://kiran:kiran@localhost:3308/fastapi"
 
 engine = create_engine(
